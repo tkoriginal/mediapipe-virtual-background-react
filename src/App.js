43e0da5bafdb8b348a7e39757fd33d8d374f288c
Tauqueer Khan <tauqueer@talky.io>
2022-02-23 22:36:55 -0500

    MediaPipe Virtual Background with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import { SelfieSegmentation } from "@mediapipe/selfie_segmentation";
+import "./App.css";
 
 function App() {
+  const inputVideoRef = useRef();
+  const canvasRef = useRef();
+  const contextRef = useRef();
+
+  useEffect(() => {
+    contextRef.current = canvasRef.current.getContext("2d");
+    const constraints = {
+      video: { width: { min: 1280 }, height: { min: 720 } },
+    };
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      inputVideoRef.current.srcObject = stream;
+      sendToMediaPipe();
+    });
+
+    const selfieSegmentation = new SelfieSegmentation({
+      locateFile: (file) =>
+        `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`,
+    });
+
+    selfieSegmentation.setOptions({
+      modelSelection: 1,
+      selfieMode: true,
+    });
+
+    selfieSegmentation.onResults(onResults);
+
+    const sendToMediaPipe = async () => {
+      if (!inputVideoRef.current.videoWidth) {
+        console.log(inputVideoRef.current.videoWidth);
+        requestAnimationFrame(sendToMediaPipe);
+      } else {
+        await selfieSegmentation.send({ image: inputVideoRef.current });
+        requestAnimationFrame(sendToMediaPipe);
+      }
+    };
+  }, []);
+
+  const onResults = (results) => {
+    contextRef.current.save();
+    contextRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    contextRef.current.drawImage(
+      results.segmentationMask,
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    // Only overwrite existing pixels.
+    contextRef.current.globalCompositeOperation = "source-out";
+    contextRef.current.fillStyle = "#00FF00";
+    contextRef.current.fillRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+
+    // Only overwrite missing pixels.
+    contextRef.current.globalCompositeOperation = "destination-atop";
+    contextRef.current.drawImage(
+      results.image,
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+
+    contextRef.current.restore();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay ref={inputVideoRef} />
+      <canvas ref={canvasRef} width={1280} height={720} />
     </div>
   );
 }
